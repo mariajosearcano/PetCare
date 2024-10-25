f4c9447745d376eedd28ec3964bc833a225ad9a5
mariajosearcano <mariajosearcano@outlook.com>
2024-10-25 09:38:55 -0500

    01-01-01
    (feature) add db controller

--- index.js
+++ index.js
@@ -1,47 +1,12 @@
-const express = require('express');
-const { Pool } = require('pg');
-const bodyParser = require('body-parser');
-
-// Inicializa la aplicación Express
-const app = express();
-
-// Middleware para procesar los datos enviados por el formulario (POST)
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Configuración de la conexión a PostgreSQL
-const pool = new Pool({
-  user: 'postgres',        
-  host: 'localhost',         // Host donde corre PostgreSQL
-  database: 'bbd_owner',     // Cambia por el nombre de tu base de datos
-  password: 'madiar2003',    // Cambia por tu contraseña de PostgreSQL
-  port: 5432,                // Puerto por defecto de PostgreSQL
-});
-
-// Ruta para servir el formulario (index.html)
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/src/html/formsPetOwner.html');  // Cambia esta ruta según la ubicación de tu HTML
-});
-
-// Ruta para manejar el envío del formulario
-app.post('/submit-form', async (req, res) => {
-  const { name, lastName, document, email, address, phone, password } = req.body;
-
-  try {
-    // Inserta los datos en la tabla 'pet-owner'
-    const result = await pool.query(
-      'INSERT INTO "pet-owner" (name, last_name, id, email, address, phone_number, password) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-      [name, lastName, document, email, address, phone, password]
-    );
-    res.send('Datos insertados correctamente');
-  } catch (err) {
-    console.error(err);
-    res.send('Error al insertar los datos');
-  }
-});
-
-
-// Iniciar el servidor
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+import { app } from './app.js'
+import { sequelize } from './database/connection.js'
+try {
+  await sequelize.authenticate()
+  console.log('Connection has been established successfully')
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`app running in port ${PORT}`)
+  })
+} catch (error) {
+  console.error('Error connecting to the database', error)
+}
