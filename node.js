83911c9189a6b40078efcfcbe9a28bfcbfe47805
mariajosearcano <mariajosearcano@outlook.com>
2024-11-03 22:53:07 -0500

    01-01-01
    (fix) repeat id

--- node.js
+++ node.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 //rutas
 app.use('/', router);
 app.use(express.static('src'));
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
 app.use('/src/css', express.static(path.resolve(__dirname, 'src/css')));
 app.use('/src/html', express.static(path.resolve(__dirname, 'src/html')));
 
@@ -78,16 +78,16 @@ app.listen(3007, () => {
 // });
 
 //crear una persona
-app.post('/petowner/create', (req, res) => {
-    const { document,name,last_name,email,password,phone_number } = req.body;
-    const sql = 'INSERT INTO pet_owner (document,name,last_name,email,password,phone_number) VALUES (?, ?, ?, ?, ?, ?)';
-    conexion.query(sql, [document,name,last_name,email,password,phone_number], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.json({ id: result.insertId, document,name,last_name,email,password,phone_number });
-    });
-});
+// app.post('/petowner/create', (req, res) => {
+//     const { document,name,last_name,email,password,phone_number } = req.body;
+//     const sql = 'INSERT INTO pet_owner (document,name,last_name,email,password,phone_number) VALUES (?, ?, ?, ?, ?, ?)';
+//     conexion.query(sql, [document,name,last_name,email,password,phone_number], (err, result) => {
+//         if (err) {
+//             return res.status(500).send(err);
+//         }
+//         res.json({ id: result.insertId, document,name,last_name,email,password,phone_number });
+//     });
+// });
 
 // // obtener una persona
 // app.get('/person/read/:document', (req, res) => {
