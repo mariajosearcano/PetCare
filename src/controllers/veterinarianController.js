1e85285cb1e117fda2f484212f261f7d591a700d
mariajosearcano <mariajosearcano@outlook.com>
2024-11-06 22:52:52 -0500

    01-01-01
    (add) update and delete functionalities, and kiut alerts

--- src/controllers/veterinarianController.js
+++ src/controllers/veterinarianController.js
@@ -4,9 +4,10 @@ async function getVeterinarians(req, res) {
     connection.query('SELECT * FROM veterinarian', (err, results) => {
         if (err) {
             console.error(err);
-            res.status(500).send('Error al obtener los datos');
+            return res.status(500).send('Error getting veterinary users');
         } else {
-            res.json(results);
+            //console.log('Veterinary users successfully obtained');
+            return res.json(results);
         }
     });
 }
@@ -23,14 +24,41 @@ async function putVeterinarian(req, res) {
 
     connection.query(sql, [newPutDocument, newPutName, newPutLastName, newPutEmail, newPutPassword, newPutPhoneNumber, putDocument], (err, result) => {
         if (err) {
-            return res.status(500).send(err);
+            console.error(err)
+            return res.status(500).send('Veterinary user not updated');
         }
 
-        res.redirect('/manageUsers');
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Veterinary user not found' });
+        }
+
+        return res.json({ message: 'Veterinary user updated successfully' });
+    });
+}
+
+async function deleteVeterinarian(req, res) {
+    const { document } = req.body;
+
+    const sql = `
+        DELETE FROM veterinarian WHERE document = ?
+    `;
+
+    connection.query(sql, [document], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Veterinarian user not deleted');
+        }
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Veterinarian user not found' });
+        }
+
+        return res.json({ message: 'Veterinarian user deleted successfully' });
     });
 }
 
 module.exports = {
     getVeterinarians,
-    putVeterinarian
+    putVeterinarian,
+    deleteVeterinarian
 }
