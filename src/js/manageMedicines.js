8fba3bd4a6c78698a46a79d15ebe0003b74695b4
mariajosearcano <mariajosearcano@outlook.com>
2024-11-07 21:10:01 -0500

    01-01-01
    (add) more characters to the email and password regex

---
+++ src/js/manageMedicines.js
@@ -0,0 +1,27 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    collapse();
+});
+
+function collapse() {
+    const collapseButtons = document.querySelectorAll('[data-bs-toggle="collapse"]');
+    
+    collapseButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const targetId = this.getAttribute('data-bs-target');
+
+            collapseButtons.forEach(btn => {
+                const otherTargetId = btn.getAttribute('data-bs-target');
+                if (otherTargetId !== targetId) {
+                    const collapseElement = document.querySelector(otherTargetId);
+                    const collapse = bootstrap.Collapse.getInstance(collapseElement);
+                    if (collapse) {
+                        collapse.hide();
+                    }
+                }
+            });
+        });
+    });
+}
