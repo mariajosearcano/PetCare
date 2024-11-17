92a3149216fbdcce84c210fc364ae24549eea449
David C <dcorreap97@gmail.com>
2024-11-17 00:12:46 -0500

    feature(frontend)
    add invalid feedback message

--- src/js/managePet.js
+++ src/js/managePet.js
@@ -54,6 +54,26 @@ async function InsertPet() {
     inputPetOwnerDocument.value = '';
 }
 
+// validacion de campos
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
 // control collapse
 document.addEventListener('DOMContentLoaded', function () {
     const collapseButtons = document.querySelectorAll('[data-bs-toggle="collapse"]');
